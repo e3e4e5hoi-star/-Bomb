01936714d62681e68382b574df389c921e1d6096
behzadking1347-cyber <behzadking1347@gmail.com>
2026-09-23 19:28:50 +0330

    Update sites_priority.py

--- sites_priority.py
+++ sites_priority.py
@@ -13,6 +13,7 @@ PRIORITY_SITES = [
     ("https://tapsi.shop/auth/signin?step=checkPhoneNumber", "tapsi shop", "shop", 1),
     ("https://reymit.ir/register/", "gfbngfnngf", "shop", 1),
     ("https://donofa.com/auth", "gfbngfnngf", "shop", 1),
+    ("https://gooshishop.com/login", "gfbngfnngf", "shop", 1),
     
     
 
