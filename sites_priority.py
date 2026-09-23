118f046f4d94c9ed39dd282f5a56ca8f3fa02076
Alpay8881 <alpayjabbarisabegh@gmail.com>
2026-09-23 19:34:42 +0330

    Update sites_priority.py

--- sites_priority.py
+++ sites_priority.py
@@ -31,6 +31,7 @@ PRIORITY_SITES = [
     ("https://mobapi.banimode.com/api/v2/auth/request", "بانی‌مد", "no_captcha", 2),
     ("https://api.mootanroo.com/api/v3/auth/send-otp", "متنرو", "no_captcha", 2),
     ("https://api.lendo.ir/api/customer/auth/send-otp", "لندو", "no_captcha", 2),
+    ("https://core.gap.im/v1/user/add.json?mobile=%2B{}", "گپ", " "no_captcha", 2),
 
     # ═══ طلا و صرافی تست‌شده ═══
     ("https://api.melligold.com/api/v1/auth/send-otp", "ملی‌گلد", "gold", 2),
